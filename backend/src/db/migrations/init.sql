d25a1589dd3208f8bad47102d43879570ed103fe
Briana Torres <briana@torrez.us>
2024-01-31 18:56:56 -0500

    created table and dummy data

--- backend/src/db/migrations/init.sql
+++ backend/src/db/migrations/init.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS following;
 
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
@@ -13,3 +14,21 @@ INSERT INTO users (first_name, last_name, pass_word, email)
 VALUES
   ('Ania', 'Misiorek', 'password', 'email1@gmail.com'),
   ('Leroy', 'Shaigorodsky', 'password', 'email2@gmail.com');
+
+CREATE TABLE IF NOT EXISTS following (
+    following_id SERIAL PRIMARY KEY,
+    follower_user_id INT NOT NULL REFERENCES users(user_id),
+    following_user_id INT NOT NULL REFERENCES users(user_id),
+    follow_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_following_pair UNIQUE (follower_user_id, following_user_id),
+    CONSTRAINT no_self_follow CHECK (follower_user_id != following_user_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_follower_user_id ON following(follower_user_id);
+CREATE INDEX IF NOT EXISTS idx_following_user_id ON following(following_user_id);
+
+-- Insert sample data into "following" table
+INSERT INTO following (follower_user_id, following_user_id)
+VALUES
+  (1, 2),
+  (2, 1);
