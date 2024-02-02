0c5220fc60f923cca51385da199ac57918dbeac9
DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
2024-02-02 04:43:44 -0500

    implemeneted bottom nav with routing to empty pages

--- frontend/pages/AuthPage.tsx
+++ frontend/pages/AuthPage.tsx
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native-paper'
+import { useNavigation } from '@react-navigation/native'
 
 const AuthPage = () => {
+  const navigation = useNavigation()
+
+  // router the user to the feed page
+  const onClicked = async () => {
+    await Alert.alert('Signed In')
+    navigation.navigate('FeedPage')
+  }
+
   return (
     <View>
-      <Button>Sign Up</Button>
-      <Button>Log In</Button>
+      <Button onPress={onClicked}>Sign Up</Button>
+      <Button onPress={onClicked}>Log In</Button>
     </View>
   )
 }
