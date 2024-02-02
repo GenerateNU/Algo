369c900f9619293bb24b796bfbdaf3776562c995
Jakob Philippe <philippe.j@northeastern.edu>
2024-02-02 12:12:33 -0500

    added oauth_tokens to init.sql

--- backend/src/db/migrations/init.sql
+++ backend/src/db/migrations/init.sql
@@ -8,6 +8,16 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS oauth_tokens (
+    oauth_token_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    request_token VARCHAR,
+    request_secret VARCHAR,
+    access_token VARCHAR,
+    access_secret VARCHAR,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
 -- Insert sample data into "users" table
 INSERT INTO users (first_name, last_name, pass_word, email)
 VALUES
