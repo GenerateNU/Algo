6c9c348451c8b0d0fc0c52ee3c17fd99ca61aa4b
Ania Misiorek <misiorek.a@northeastern.edu>
2024-02-06 18:18:07 -0500

    linting

--- backend/src/services/user.go
+++ backend/src/services/user.go
@@ -26,58 +26,57 @@ func (us *UserService) GetAllUsers() ([]models.User, error) {
 	return users, nil
 }
 
-func (us *UserService) CreateLongTermGoalForUser(userID uint, longTermGoal string) (models.UserLongTermGoals, error) {
-    goal := models.UserLongTermGoals{
-        UserID:       userID,
-        LongTermGoal: models.LongTermGoal(longTermGoal),
-    }
+func (us *UserService) CreateLongTermGoalForUser(userID uint, longTermGoalID uint) (models.UserLongTermGoals, error) {
+	goal := models.UserLongTermGoals{
+		UserID:         userID,
+		LongTermGoalID: longTermGoalID,
+	}
 
-    if err := us.DB.Create(&goal).Error; err != nil {
-        return models.UserLongTermGoals{}, err
-    }
+	if err := us.DB.Create(&goal).Error; err != nil {
+		return models.UserLongTermGoals{}, err
+	}
 
-    return goal, nil
+	return goal, nil
 }
 
 func (us *UserService) GetLongTermGoalsForUser(userID uint) ([]models.UserLongTermGoals, error) {
-    var userLongTermGoals []models.UserLongTermGoals
-    if err := us.DB.Where("user_id = ?", userID).Find(&userLongTermGoals).Error; err != nil {
-        return nil, err
-    }
-    return userLongTermGoals, nil
+	var userLongTermGoals []models.UserLongTermGoals
+	if err := us.DB.Where("user_id = ?", userID).Find(&userLongTermGoals).Error; err != nil {
+		return nil, err
+	}
+	return userLongTermGoals, nil
 }
 
-func (us *UserService) UpdateLongTermGoalForUser(userID uint, goalID uint, longTermGoal string) (models.UserLongTermGoals, error) {
-    var userLongTermGoal models.UserLongTermGoals
-    if err := us.DB.First(&userLongTermGoal, goalID).Error; err != nil {
-        return models.UserLongTermGoals{}, err
-    }
+func (us *UserService) UpdateLongTermGoalForUser(userID uint, goalID uint, longTermGoalID uint) (models.UserLongTermGoals, error) {
+	var userLongTermGoal models.UserLongTermGoals
+	if err := us.DB.First(&userLongTermGoal, goalID).Error; err != nil {
+		return models.UserLongTermGoals{}, err
+	}
 
-    if userLongTermGoal.UserID != userID {
-        return models.UserLongTermGoals{}, errors.New("User does not have permission to update this goal")
-    }
+	if userLongTermGoal.UserID != userID {
+		return models.UserLongTermGoals{}, errors.New("User does not have permission to update this goal")
+	}
 
-    userLongTermGoal.LongTermGoal = models.LongTermGoal(longTermGoal)
-    if err := us.DB.Save(&userLongTermGoal).Error; err != nil {
-        return models.UserLongTermGoals{}, err
-    }
+	if err := us.DB.Save(&longTermGoalID).Error; err != nil {
+		return models.UserLongTermGoals{}, err
+	}
 
-    return userLongTermGoal, nil
+	return userLongTermGoal, nil
 }
 
 func (us *UserService) DeleteLongTermGoalForUser(userID uint, goalID uint) error {
-    var userLongTermGoal models.UserLongTermGoals
-    if err := us.DB.First(&userLongTermGoal, goalID).Error; err != nil {
-        return err
-    }
+	var userLongTermGoal models.UserLongTermGoals
+	if err := us.DB.First(&userLongTermGoal, goalID).Error; err != nil {
+		return err
+	}
 
-    if userLongTermGoal.UserID != userID {
-        return errors.New("User does not have permission to delete this goal")
-    }
+	if userLongTermGoal.UserID != userID {
+		return errors.New("User does not have permission to delete this goal")
+	}
 
-    if err := us.DB.Delete(&userLongTermGoal).Error; err != nil {
-        return err
-    }
+	if err := us.DB.Delete(&userLongTermGoal).Error; err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
