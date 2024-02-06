687677adba810eb63e3c4fe0bbd9ed02c2fe1b53
Ania Misiorek <misiorek.a@northeastern.edu>
2024-02-06 02:34:00 -0500

    changes

--- backend/src/models/user_long_term_goals.go
+++ backend/src/models/user_long_term_goals.go
@@ -4,18 +4,10 @@ import (
 	"backend/src/types"
 )
 
-type LongTermGoal string
-
-const (
-	GainFollowers          		LongTermGoal = "Gain followers"
-	SaveForRetirement      		LongTermGoal = "Save for retirement"
-	SaveForChildrensEducation 	LongTermGoal = "Save for children’s education"
-	SaveForDownPayment   	  	LongTermGoal = "Save for a down payment"
-)
-
 type UserLongTermGoals struct {
 	types.Model
-	LongTermGoal  LongTermGoal  `gorm:"type:varchar(100);not null" json:"long_term_goal"`
-	UserID        uint          `gorm:"not null" json:"user_id" validate:"required"`
-	User       	  User			`gorm:"foreignKey:UserID" validate:"required"`
+	UserID         uint          `gorm:"not null" json:"user_id"`
+	User           *User         `gorm:"foreignKey:UserID"`
+	LongTermGoalID uint          `json:"long_term_goal_id"`
+	LongTermGoal   *LongTermGoal `gorm:"foreignkey:LongTermGoalID"`
 }
