d9683acd23ee63640ae4b3edfad28f600e478ab1
Adam Ma <adam.zym@gmail.com>
2024-02-07 19:10:53 -0500

    I'm The Goat Part 3

--- backend/src/models/user_short_term_goals.go
+++ backend/src/models/user_short_term_goals.go
@@ -8,6 +8,6 @@ type UserShortTermGoals struct {
 	types.Model
 	UserID         	uint         	`gorm:"not null" json:"user_id"`
 	User           	*User         	`gorm:"foreignKey:UserID"`
-	ShortTermGoalID uint          	`json:short_term_goal_id"`
+	ShortTermGoalID uint          	`json:"short_term_goal_id"`
 	ShortTermGoal   *ShortTermGoal 	`gorm:"foreignkey:ShortTermGoalID"`
 }
