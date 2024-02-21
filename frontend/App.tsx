779d06c2b923e23bbfa7a80e42913a800208a9f8
JLetsGitIt <86565950+JLetsGitIt@users.noreply.github.com>
2024-02-21 06:14:31 -0500

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,28 +4,35 @@ import { getAllUsers } from './services/users';
 import { useEffect, useState } from 'react';
 import { User } from './types/types';
 import Logo from './assets/verified.svg';
+import Svg, { Path } from 'react-native-svg';
+import React from 'react';
+
 
 export default function App() {
   const [users, setUsers] = useState<User[]>();
   useEffect(() => {
-    getAllUsers().then((data) => setUsers(data))
-  }, [])
+    getAllUsers().then(data => setUsers(data));
+  }, []);
+  const logoXml = Logo ? Logo.toString() : '';
   return (
     <View style={styles.container}>
-      <Text className="font-bold mb-2 w-full text-left" >Open up App.js to start working on your app!</Text>
-      <Logo width={120} height={40} />;
-      {
-        users &&
-        <View className='w-full'>
-          {
-            users.map((user, index) => (
-              <Text key={index} className="pb-2 text-left w-100">
-                {`First Name: ${user.first_name}\n`}{`Last Name: ${user.last_name}`}
-              </Text>
-            ))
-          }
+      {/* <Text className="font-bold mb-2 w-full text-left"> */}
+      <Text style={{ fontWeight: 'bold', marginBottom: 2, width: '100%', textAlign: 'left' }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Logo width={120} height={40} />
+      {users && (
+        // <View className="w-full">
+        <View style={{ width: '100%' }}>
+          {users.map((user, index) => (
+            // <Text key={index} className="pb-2 text-left w-100">
+            <Text key={index} style={{ paddingBottom: 2, textAlign: 'left', width: '100%' }}>
+              {`First Name: ${user.first_name}\n`}
+              {`Last Name: ${user.last_name}`}
+            </Text>
+          ))}
         </View>
-      }
+      )}
       <StatusBar style="auto" />
     </View>
   );
