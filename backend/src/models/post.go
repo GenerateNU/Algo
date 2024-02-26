906713f403968a49e211cb8c46e9adfc5b8bfa80
Adam Ma <adam.zym@gmail.com>
2024-02-25 21:56:35 -0500

    Fix

--- backend/src/models/post.go
+++ backend/src/models/post.go
@@ -20,7 +20,7 @@ type Post struct {
 	PostType     PostType       `gorm:"type:post_type_enum;not null" json:"post_type"`
 	NumData      float64        `gorm:"not null" json:"num_data"`
 	TickerSymbol string         `gorm:"type:varchar(9);" json:"ticker_symbol"`
-	TimeStamp    time.Time      `gorm:"default:CURRENT_TIMESTAMP" json:"time_stamp"`
+	TimeStamp    time.Time      `gorm:"default:created_at" json:"time_stamp"`
 	Comment      string         `gorm:"not null" json:"comment"`
 	Title        string         `gorm:"not null" json:"title"`
 }
