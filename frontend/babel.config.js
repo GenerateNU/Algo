eac7ceeb7d0871e569d02e5b940c6ad9371dece9
leoRysing <leshaigor@gmail.com>
2024-04-09 19:42:46 -0400

    Animation working / confirmation page completed

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,12 +3,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel", 
+    plugins: [
+      "nativewind/babel", 
       ["module:react-native-dotenv", {
         "moduleName": "@env",
         "path": "../.env",
         }
-      ]
+      ],
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
     ],
   };
 }
