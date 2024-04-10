b61d54e4074641444e04f18f87913581e1e8f167
Cam Plume <116120547+CamPlume1@users.noreply.github.com>
2024-04-10 13:49:18 -0400

    Update Leader Model

--- backend/src/models/leader.go
+++ backend/src/models/leader.go
@@ -1,8 +1,8 @@
 package models
 
 type Leader struct {
-	FollowingUserID uint `gorm:"type:int;" json:"following_user_id"`
-	FollowerCount   uint `gorm:"type:int;" json:"follower_count"`
+	FollowingUserID string `gorm:"type:varchar(255);" json:"following_user_id"`
+	FollowerCount   string `gorm:"type:varchar(255);" json:"follower_count"`
 
 	//Must be Preloaded
 	FollowingUser User `gorm:"foreignKey:FollowingUserID;references:ID" json:"leader_user"`
