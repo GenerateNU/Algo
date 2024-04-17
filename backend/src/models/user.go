02025c73298d4a74aac77888696d3b840d86d7d5
Jakob Philippe <jakob.philippe@gmail.com>
2024-04-17 17:19:38 -0400

    backend changes

--- backend/src/models/user.go
+++ backend/src/models/user.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type User struct {
-	ID        string    `gorm:"column:id;primaryKey;"`
+	ID        string    `gorm:"column:id;primaryKey;" json:"id"`
 	CreatedAt time.Time `json:"created_at" example:"2023-09-20T16:34:50Z"`
 	UpdatedAt time.Time `json:"updated_at" example:"2023-09-20T16:34:50Z"`
 	FirstName string    `gorm:"type:varchar(255)" json:"first_name"`
