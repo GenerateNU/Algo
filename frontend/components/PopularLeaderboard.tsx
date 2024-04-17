3a256e9194413f95e3c6e1929934c8dbd8b0ed40
leoRysing <leshaigor@gmail.com>
2024-04-17 13:51:21 -0400

    more linter issues

--- frontend/components/PopularLeaderboard.tsx
+++ frontend/components/PopularLeaderboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Leader } from '../types/types';
 import PopularUser from './PopularUser';
 
--- frontend/components/PopularTrendingBoard.tsx
+++ frontend/components/PopularTrendingBoard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Trending } from '../types/types';
 import TrendingUser from './TrendingUser';
 
