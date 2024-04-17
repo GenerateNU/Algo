19a2a317f0fb0b034ade8afb5a0fbb9e387a051e
leoRysing <leshaigor@gmail.com>
2024-04-17 19:45:20 -0400

    eslint hopefully cleaner time three

--- frontend/components/User.tsx
+++ frontend/components/User.tsx
@@ -3,12 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { User } from '../types/types';
 import { getAllUsers } from '../services/users';
 
-// interface PostProps {
-//   name: string;
-//   postIndex: number;
-// }
-
-const Post: React.FC = ({}) => {
+const Post: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
