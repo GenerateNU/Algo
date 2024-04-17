9e2ed05e2580fdb49cb5df1f90aa3312387f280e
JLetsGitIt <86565950+JLetsGitIt@users.noreply.github.com>
2024-04-17 19:11:12 -0400

    Update PostNew.tsx

--- frontend/components/Feed/PostNew.tsx
+++ frontend/components/Feed/PostNew.tsx
@@ -1,10 +1,18 @@
 import { Text, StyleSheet, View, Image } from 'react-native';
 import { Post } from '../../types/types';
 import Vote from './Vote';
+import { SvgXml } from 'react-native-svg';
 
 type PostProps = {
   post: Post;
 };
+
+const AddSvg = `
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.99916 20C15.4774 20 20 15.4706 20 10C20 4.52138 15.4693 0 9.99123 0C4.52273 0 0 4.52138 0 10C0 15.4706 4.53067 20 9.99916 20Z" fill="#E8E8E8"/>
+<path d="M5.26315 10.012C5.26315 9.45115 5.63621 9.05385 6.16426 9.05385H8.84484V6.22236C8.84484 5.66667 9.21532 5.26318 9.73751 5.26318C10.2688 5.26318 10.6452 5.66667 10.6452 6.22236V9.05385H13.335C13.8554 9.05385 14.236 9.45115 14.236 10.012C14.236 10.5651 13.8554 10.9464 13.335 10.9464H10.6452V13.7859C10.6452 14.3415 10.2688 14.7354 9.73751 14.7354C9.21532 14.7354 8.84484 14.3336 8.84484 13.7859V10.9464H6.16426C5.63621 10.9464 5.26315 10.5651 5.26315 10.012Z" fill="#02AD98"/>
+</svg>
+`;
 
 const PostNew: React.FC<PostProps> = ({ post }) => {
 
@@ -79,6 +87,11 @@ const styles = StyleSheet.create({
     top: 60,
     width: 20,
     height: 20,
+    zIndex: 1,
+  },
+    add: {
+    bottom: 15,
+    left: 25,
     zIndex: 1,
   }
 });
